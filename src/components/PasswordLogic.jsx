42113df4579453f008e5d809415baf346d71d7b4
bilalansar3 <doctorofseoandcodding@gmail.com>
2025-03-24 12:18:47 +0500

    password

--- src/components/PasswordLogic.jsx
+++ src/components/PasswordLogic.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PasswordLogic = () => {
+    const [show,setShow]=useState(true);
   return (
     <div>
       <h1>Hide Show Logic Page</h1>
+      <input type="password" placeholder='password' onChange={(e)=>setShow(e.target.value)}  />
+      <button className='btn btn-primary'>{setShow? "show":"hide"}</button>
     </div>
   )
 }
